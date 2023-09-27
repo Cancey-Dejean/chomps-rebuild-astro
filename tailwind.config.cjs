85c43a3e6a7e24fca79fdd346f54cf0dc17f4c47
Cancey-Dejean <you@example.com>
2023-09-26 21:22:55 -0500

    Added Hero Section

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,10 +6,13 @@ module.exports = {
       fontFamily: {
         heading: ['Mindset', 'helvetica', 'arial', 'sans-serif'],
         body: ['Franklin Gothic Book', 'helvetica', 'arial', 'sans-serif'],
+        button: ['Franklin Gothic Md', 'helvetica', 'arial', 'sans-serif'],
         secondary: ['Colby', 'helvetica', 'arial', 'sans-serif']
       },
       colors: {
-        primary: '#d02c2f'
+        primary: '#d02c2f',
+        bg: '#FEF5F0',
+        'baby-blue': '#c9ebf7'
         // primary: {
         //   50: '#F2F2F2',
         //   100: '#E0E0E0',
@@ -28,7 +31,28 @@ module.exports = {
       },
       gap: {
         15: '1.5rem'
+      },
+      screens: {
+        xs: '374px'
       }
+      // fontSize: {
+      //   heading: [
+      //     '4.8rem',
+      //     {
+      //       lineHeight: '4.8rem',
+      //       letterSpacing: '0rem',
+      //       fontWeight: '700'
+      //     }
+      //   ],
+      //   subtitle: [
+      //     '2.4rem',
+      //     {
+      //       lineHeight: '2.8rem',
+      //       letterSpacing: '0rem',
+      //       fontWeight: '400'
+      //     }
+      //   ]
+      // }
     }
   },
   plugins: []
